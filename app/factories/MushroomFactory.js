3fe74c1f9eb12971fa160961988b8af8b9446fb0
daniel-certa-1228 <mfdancerta@gmail.com>
2017-08-30 20:26:58 -0500

    fincshed factory and controller for mushroom dsplay

--- app/factories/MushroomFactory.js
+++ app/factories/MushroomFactory.js
@@ -1,2 +1,21 @@
 "use strict";
 console.log( "MushroomFactory.js" );
+
+app.factory("MushroomFactory", function($q, $http){
+
+	let getMushrooms = () => {
+		let items = [];
+		return $q ((resolve, reject) => {
+			$http.get('https://third-tester.firebaseio.com/mushrooms.json')
+		.then((itemObject) => {
+			let itemCollection = itemObject.data;
+			resolve(itemCollection);
+			console.log( "itemCollection", itemCollection );
+			})
+		.catch((error) => {
+			reject(error);
+			});
+		});
+	};
+	return{getMushrooms};
+});
